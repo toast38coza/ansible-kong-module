47a28f57f3ec7a6345fda76d042c27287a4ee9ea
toast38coza <info@38.co.za>
2016-02-04 13:43:04 +0200

    Adds ability to configure a consumer for a plugin. Playbook now mirrors Kongs getting started tutorial

--- library/kong_consumer.py
+++ library/kong_consumer.py
@@ -28,6 +28,12 @@ class KongConsumer:
     	url = "{}/{}" . format (self.base_url, id)
     	return requests.delete(url)
 
+    def configure_for_plugin(self, username_or_id, api, data):
+        """This could possibly go in it's own plugin"""
+
+        url = "{}/{}/{}" . format (self.base_url, username_or_id, api)
+        return requests.post(url, data)
+
 class ModuleHelper:
     
     def get_module(self):
@@ -36,7 +42,9 @@ class ModuleHelper:
             kong_admin_uri = dict(required=True, type='str'),
             username = dict(required=False, type='str'),
             custom_id = dict(required=False, type='str'),
-            state = dict(required=False, default="present", choices=['present', 'absent', 'list'], type='str'),    
+            state = dict(required=False, default="present", choices=['present', 'absent', 'list', 'configure'], type='str'),    
+            data = dict(required=False, type='dict'),
+            api_name = dict(required=False, type='str'),
         )
         return AnsibleModule(argument_spec=args,supports_check_mode=False)
 
@@ -45,19 +53,20 @@ class ModuleHelper:
         state = module.params['state']    
         username = module.params.get('username', None)
         custom_id = module.params.get('custom_id', None)
+        data = module.params.get('data', None)
+        api_name = module.params.get('api_name', None)
         
-        return (url, username, custom_id, state)
+        return (url, username, custom_id, state, api_name, data)
 
     def get_response(self, response, state):
 
-        if state == "present":
+        if state in ["present", "configure"]:
             meta = json.dumps(response.content)
             has_changed = response.status_code == 201
             
         if state == "absent":
             meta = {}
             has_changed = response.status_code == 204
-
         if state == "list":
             meta = response.json()
             has_changed = False
@@ -66,21 +75,19 @@ class ModuleHelper:
 
 def main():
 
-    state_to_method = {
-        "present": "add",
-        "absent": "delete"
-    }
     helper = ModuleHelper()
 
     global module # might not need this
     module = helper.get_module()  
-    base_url, username, id, state = helper.prepare_inputs(module)
+    base_url, username, id, state, api_name, data = helper.prepare_inputs(module)
 
     api = KongConsumer(base_url)
     if state == "present":
         response = api.add(username, id)
     if state == "absent":
         response = api.delete(username)
+    if state == "configure":
+        response = api.configure_for_plugin(username, api_name, data)
     if state == "list":
         response = api.list()
     
--- library/test_kong_consumer.py
+++ library/test_kong_consumer.py
@@ -1,8 +1,13 @@
 import unittest, responses, requests, json, mock
+from mock import call
 from urlparse import parse_qsl, parse_qs
 from kong_consumer import KongConsumer, ModuleHelper, main
 
 from ansible.module_utils.basic import *
+
+# python3 compatible imports:
+from six.moves.urllib.parse import parse_qsl, parse_qs
+
 
 
 mock_kong_admin_url = "http://192.168.99.100:8001"
@@ -36,11 +41,26 @@ class KongPluginTestCase(unittest.TestCase):
 		response = self.api.add(username='joesoap')
 		assert response.status_code == 201
 
+
+	@responses.activate
+	def test_configure_for_plugin(self):
+
+		expected_url = "{}/consumers/joe/auth-key" . format (mock_kong_admin_url)
+		responses.add(responses.POST, expected_url, status=201)
+
+		data = { "key": "123" }
+		response = self.api.configure_for_plugin("joe", "auth-key", data)
+
+		assert response.status_code == 201
+
+		body = parse_qs(responses.calls[0].request.body)
+		body_exactly = parse_qsl(responses.calls[0].request.body)
+		assert body['key'][0] == "123", \
+			"Expect correct. data to be sent. Got: {}" . format (body_exactly)
+
 	@responses.activate 
 	def test_add_invalid_inputs(self):
 		self.assertRaises(AssertionError, self.api.add)
-
-
 
 class ModuleHelperTestCase(unittest.TestCase):
 
@@ -54,7 +74,7 @@ class ModuleHelperTestCase(unittest.TestCase):
 	@mock.patch.object(ModuleHelper, 'prepare_inputs')
 	def test_main_add(self, mock_prepare_inputs, mock_module, mock_add, mock_exit_json, mock_get_response):
 
-		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "present")
+		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "present", None, None)
 		mock_get_response.return_value = (True, requests.Response())
 		main()
 
@@ -67,7 +87,7 @@ class ModuleHelperTestCase(unittest.TestCase):
 	@mock.patch.object(ModuleHelper, 'prepare_inputs')
 	def test_main_delete(self, mock_prepare_inputs, mock_module, mock_delete, mock_exit_json, mock_get_response):
 
-		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "absent")
+		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "absent", None, None)
 		mock_get_response.return_value = (True, requests.Response())
 		main()
 
@@ -80,11 +100,27 @@ class ModuleHelperTestCase(unittest.TestCase):
 	@mock.patch.object(ModuleHelper, 'prepare_inputs')
 	def test_main_list(self, mock_prepare_inputs, mock_module, mock_list, mock_exit_json, mock_get_response):
 
-		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "list")
+		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "list", None, None)
 		mock_get_response.return_value = (True, requests.Response())
 		main()
 
-		assert mock_list.called				
+		assert mock_list.called			
+
+	@mock.patch.object(ModuleHelper, 'get_response')
+	@mock.patch.object(AnsibleModule, 'exit_json')
+	@mock.patch.object(KongConsumer, 'configure_for_plugin')
+	@mock.patch.object(ModuleHelper, 'get_module')
+	@mock.patch.object(ModuleHelper, 'prepare_inputs')
+	def test_main_list(self, mock_prepare_inputs, mock_module, mock_configure_for_plugin, mock_exit_json, mock_get_response):
+
+		mock_prepare_inputs.return_value = (mock_kong_admin_url, "1","joesoap", "configure", "auth-key", {"key": "123"})
+		mock_get_response.return_value = (True, requests.Response())
+		main()
+
+		assert mock_configure_for_plugin.called				
+
+		expected_call = call('1', 'auth-key', {'key': '123'})
+		assert mock_configure_for_plugin.call_args_list[0] == expected_call
 
 
 	def test_prepare_inputs(self):
@@ -97,12 +133,15 @@ class ModuleHelperTestCase(unittest.TestCase):
 			'state': 'present',
 			'username': 'joesoap',
 		}
-		url, username, id, state = self.helper.prepare_inputs(mock_module)
+		url, username, id, state, api_name, data = self.helper.prepare_inputs(mock_module)
 
 		assert url == mock_kong_admin_url
 		assert state == 'present'
 		assert username == 'joesoap'
 		assert id == None
+		assert data is None
 
 
 
+if __name__ == '__main__':
+    unittest.main()   
