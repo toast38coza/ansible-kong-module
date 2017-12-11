b46d2f59af6039a8709c906f7508caa704653e20
Timo Beckers <timo.beckers@klarrio.com>
2017-12-11 19:53:19 +0100

    kong - Kong client core: implement _put(), make all requests args named

--- module_utils/kong.py
+++ module_utils/kong.py
@@ -11,7 +11,7 @@ class Kong(object):
         'plugins'
     ]
 
-    def __init__(self, base_url, auth_user=None, auth_pass=None):
+    def __init__(self, base_url, auth_user=None, auth_pass=None, ping=True):
 
         self.base_url = base_url
 
@@ -21,20 +21,24 @@ class Kong(object):
         if auth_user is not None and auth_pass is not None:
             self.auth = (auth_user, auth_pass)
 
-    def _get(self, resource, action):
+        # self-check by making status call to Kong
+        if ping and self.status:
+            return
+
+    def _get(self, resource, action=None, params=None):
         """
         Execute GET request using the resource and action.
         """
         url = self._url(resource, action)
 
-        r = requests.get(url, auth=self.auth)
+        r = requests.get(url, params=params, auth=self.auth)
 
         # Expect 200 OK
         r.raise_for_status()
 
         return r.json()
 
-    def _post(self, resource, action, data):
+    def _post(self, resource, action=None, data=None):
         """
         Execute POST request using the resource, action and payload.
         """
@@ -48,7 +52,7 @@ class Kong(object):
 
         return r.json()
 
-    def _patch(self, resource, action, data):
+    def _patch(self, resource, action=None, data=None):
         """
         Execute PATCH request using the resource, action and payload.
         """
@@ -61,7 +65,24 @@ class Kong(object):
 
         return r.json()
 
-    def _delete(self, resource, action):
+    def _put(self, resource, action=None, data=None):
+        """
+        Execute PUT request using the resource, action and payload.
+        """
+        url = self._url(resource, action)
+
+        r = requests.put(url, data=data, auth=self.auth)
+
+        if r.status_code == requests.codes.created:
+            return True
+
+        # Raise if status is not 200 OK
+        r.raise_for_status()
+
+        # Report no change
+        return False
+
+    def _delete(self, resource, action=None):
         """
         Execute DELETE request using the resource and action.
         """
