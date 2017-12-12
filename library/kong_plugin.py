3d55752b3868c6513fae60af155047904098e931
Timo Beckers <timo.beckers@klarrio.com>
2017-12-12 18:22:31 +0100

    kong - kong_plugin: re-write Ansible plugin on KongPlugin library

--- library/kong_plugin.py
+++ library/kong_plugin.py
@@ -1,135 +1,161 @@
-#!/usr/bin/python
-
 import requests
+from ansible.module_utils.kong_plugin import KongPlugin
+from ansible.module_utils.kong_helpers import *
+from ansible.module_utils.basic import AnsibleModule
 
-class KongPlugin:
-
-    def __init__(self, base_url, api_name, auth_username=None, auth_password=None):
-        self.base_url = "{}/apis/{}/plugins" . format(base_url, api_name)
-        if auth_username is not None and auth_password is not None:
-            self.auth = (auth_username, auth_password)
-        else:
-            self.auth = None
-        self.api = api_name
-
-    def list(self):
-        
-        return requests.get(self.base_url, auth=self.auth)
-
-    def _get_plugin_id(self, name, plugins_list):
-        """Scans the list of plugins for an ID. 
-        returns None if no matching name is found"""
-
-        for plugin in plugins_list:
-            if plugin.get("name") == name:
-                return plugin.get("id")
-
-        return None
-
-    def add_or_update(self, name, config=None):
-        
-        # does it exist already?
-        plugins_response = self.list()
-        plugins_list = plugins_response.json().get('data', [])
-
-        data = {
-            "name": name,
-        }
-        if config is not None:
-            data.update(config)   
-
-        plugin_id = self._get_plugin_id(name, plugins_list)
-        if plugin_id is None:            
-            return requests.post(self.base_url, data, auth=self.auth)
-        else:
-            url = "{}/{}" . format (self.base_url, plugin_id)
-            return requests.patch(url, data, auth=self.auth)
-
-    def delete(self, id):
-
-        url = "{}/{}" . format (self.base_url, id)
-        return requests.delete(url, auth=self.auth)
+from ansible.module_utils.dotdiff import dotdiff
 
 
-class ModuleHelper:
-    
-    def get_module(self):
+DOCUMENTATION = '''
+---
+module: kong_plugin
+short_description: Configure a Kong Plugin object.
+'''
 
-        args = dict(
-            kong_admin_uri = dict(required=True, type='str'),
-            kong_admin_username = dict(required=False, type='str'),
-            kong_admin_password = dict(required=False, type='str'),
-            api_name = dict(required=False, type='str'),
-            plugin_name = dict(required=False, type='str'),
-            plugin_id = dict(required=False, type='str'),
-            config = dict(required=False, type='dict'),
-            state = dict(required=False, default="present", choices=['present', 'absent', 'list'], type='str'),    
-        )
-        return AnsibleModule(argument_spec=args,supports_check_mode=False)
+EXAMPLES = '''
+- name: Configure key-auth on mockbin
+  kong_plugin:
+    kong_admin_uri: http://localhost:8001
+    name: key-auth
+    api_name: mockbin
+'''
 
-    def prepare_inputs(self, module):
-        url = module.params['kong_admin_uri']
-        auth_user = module.params['kong_admin_username']
-        auth_password = module.params['kong_admin_password']
-        api_name = module.params['api_name']
-        state = module.params['state']    
-        data = {
-            "name": module.params['plugin_name'], 
-            "config": module.params['config']        
-        }
+MIN_VERSION = '0.11.0'
 
-        return (url, api_name, data, state, auth_user, auth_password)
 
-    def get_response(self, response, state):
-
-        if state == "present":
-            meta = json.dumps(response.content)
-            has_changed = response.status_code == 201
-            
-        if state == "absent":
-            meta = {}
-            has_changed = response.status_code == 204
-
-        if state == "list":
-            meta = response.json()
-            has_changed = False
-
-        return (has_changed, meta)
-
-    
 def main():
 
-    state_to_method = {
-        "present": "add",
-        "absent": "delete"
-    }
-    helper = ModuleHelper()
+    ansible_module = AnsibleModule(
+        argument_spec=dict(
+            kong_admin_uri=dict(required=True, type='str'),
+            kong_admin_username=dict(required=False, type='str'),
+            kong_admin_password=dict(required=False, type='str', no_log=True),
+            name=dict(required=True, type='str'),
+            api_name=dict(required=True, type='str'),
+            consumer_name=dict(required=False, type='str', default=False),
+            config=dict(required=False, type='dict', default=dict()),
+            state=dict(required=False, default="present", choices=['present', 'absent'], type='str'),
+        ),
+        supports_check_mode=True
+    )
 
-    global module # might not need this
-    module = helper.get_module()
-    base_url, api_name, data, state, auth_user, auth_password = helper.prepare_inputs(module)
+    # Initialize output dictionary
+    result = {}
 
-    method_to_call = state_to_method.get(state)
+    # Admin endpoint & auth
+    url = ansible_module.params['kong_admin_uri']
+    auth_user = ansible_module.params['kong_admin_username']
+    auth_pass = ansible_module.params['kong_admin_password']
 
-    api = KongPlugin(base_url, api_name, auth_user, auth_password)
+    # Extract arguments
+    state = ansible_module.params['state']
+    name = ansible_module.params['name']
+    api_name = ansible_module.params['api_name']
+    consumer_name = ansible_module.params['consumer_name']
+    config = ansible_module.params['config']
+
+    # Convert consumer_name to bool if bool conversion evals to False
+    if consumer_name == 'False':
+        consumer_name = False
+
+    # Create KongAPI client instance
+    k = KongPlugin(url, auth_user=auth_user, auth_pass=auth_pass)
+
+    # Contact Kong status endpoint
+    kong_status_check(k, ansible_module)
+
+    # Kong API version compatibility check
+    kong_version_check(k, ansible_module, MIN_VERSION)
+
+    # Default return values
+    changed = False
+    resp = ''
+
+    # Check if the Plugin is already present
+    pq = k.plugin_query(name, api_name=api_name, consumer_name=consumer_name)
+
+    # ansible_module.fail_json(msg=pq)
+
+    if len(pq) > 1:
+        ansible_module.fail_json(
+            msg='Got multiple results for Plugin query name: {}, api_name: {}, consumer_name: {}'.
+                format(name, api_name, consumer_name))
+
+    # Ensure the Plugin is installed on Kong
     if state == "present":
-        response = api.add_or_update(**data)
-    if state == "absent":
-        response = api.delete(module.params['plugin_id'])
-    if state == "list":
-        response = api.list()
+        if pq:
+            # Extract the remote Plugin object into orig
+            orig = pq[0]
 
-    if response.status_code == 401:
-        module.fail_json(msg="Please specify kong_admin_username and kong_admin_password", meta=response.json())
-    elif response.status_code == 403:
-        module.fail_json(msg="Please check kong_admin_username and kong_admin_password", meta=response.json())
-    else:
-        has_changed, meta = helper.get_response(response, state)
-        module.exit_json(changed=has_changed, meta=meta)
+            # Diff the remote Plugin object against the target data if it already exists
+            plugindiff = dotdiff(orig.get('config'), config)
 
+            # Set changed flag if there's a diff
+            if plugindiff:
+                # Log modified state and diff result
+                changed = True
+                result['state'] = 'modified'
+                result['diff'] = [dict(prepared=render_list(plugindiff))]
 
-from ansible.module_utils.basic import *
-from ansible.module_utils.urls import *
+        else:
+            # We're inserting a new Plugin, set changed
+            changed = True
+            result['state'] = 'created'
+            result['diff'] = dict(
+                before_header='<undefined>', before='<undefined>\n',
+                after_header=name, after={
+                    'name': name,
+                    'consumer_name': consumer_name,
+                    'api_name': api_name,
+                    'state': 'created',
+                    'config': config
+                }
+            )
+
+        # Only make changes when Ansible is not run in check mode
+        if not ansible_module.check_mode and changed:
+            try:
+                resp = k.plugin_apply(name=name, config=config, api_name=api_name, consumer_name=consumer_name)
+            except requests.HTTPError as e:
+                ansible_module.fail_json(msg='Plugin configuration rejected by Kong.', name=name, config=config,
+                                         api_name=api_name, consumer_name=consumer_name, response=e.response._content)
+
+    # Delete the Plugin if it exists
+    if state == "absent" and pq:
+
+        # Check if the API exists
+        orig = pq[0]
+
+        # Predict a change if the Plugin exists
+        changed = True
+        result['state'] = 'deleted'
+        result['diff'] = dict(
+            before_header=name, before=orig,
+            after_header='<deleted>', after='\n'
+        )
+
+        # Only make changes when Ansible is not run in check mode
+        if not ansible_module.check_mode and orig:
+            # Issue delete call to the Kong API
+            try:
+                resp = k.plugin_delete(name, api_name=api_name, consumer_name=consumer_name)
+            except requests.HTTPError as e:
+                app_err = "Error deleting Plugin."
+                ansible_module.fail_json(msg=app_err, response=e.response._content)
+
+    # Pass through the API response if non-empty
+    if resp:
+        result['response'] = resp
+
+    # Prepare module output
+    result.update(
+        dict(
+            changed=changed,
+        )
+    )
+
+    ansible_module.exit_json(**result)
+
 
 if __name__ == '__main__':
-    main()        
+    main()
