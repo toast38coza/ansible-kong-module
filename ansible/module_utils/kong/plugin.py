97bb0dbc752c4fd6f369ef2b22ee0ee44c99276a
Timo Beckers <timo.beckers@klarrio.com>
2019-03-31 00:07:23 +0100

    kong_plugin - re-write for 1.0.0

--- ansible/module_utils/kong/plugin.py
+++ ansible/module_utils/kong/plugin.py
@@ -1,3 +1,9 @@
+"""
+ansible.module_utils.kong.plugin implements Plugin operations on the Kong Admin API.
+
+:authors: Timo Beckers
+:license: MIT
+"""
 import uuid
 
 from ansible.module_utils.kong import Kong
@@ -10,238 +16,209 @@ from ansible.module_utils.six import iteritems
 class KongPlugin(KongRoute, KongConsumer, Kong):
     """
     KongPlugin manages Plugin objects in Kong.
-    Uses KongServie, KongRoute and KongConsumer as mixins to query Services, Routes
-    and Consumers.
+
+    Uses KongRoute and KongConsumer as mixins to query Routes and Consumers.
     """
-
-    @staticmethod
-    def _prepare_config(config):
-        """
-        Takes a dictionary and prefixes the keys with 'config.'.
-        The Kong Plugin endpoint does not support a native dictionary for config.
-
-        :param config: the input config dictionary
-        :type config: dict
-        :return: dictionary with keys prefixed with 'config.'
-        :rtype: dict
-        """
-
-        return {'config.' + k: v for k, v in iteritems(config)}
 
     def plugin_list(self):
         """
         Get a list of Plugins configured in Kong.
 
-        :return: a dictionary of Plugin info
+        :return: a list of Plugins
         :rtype: dict
         """
+        return self._get_multipart('plugins')
 
-        return self._get('plugins')
+    def plugin_get(self, plugin_id):
+        """
+        Get a Plugin by its ID.
 
-    def plugin_query(self, name, service_name=None, route_attrs=None, consumer_name=None, plugin_id=None):
+        :param plugin_id: the UUID of the Plugin
+        :type plugin_id: str
+        :return: the Plugin object
+        :rtype: dict
+        """
+        return self._get(['plugins', plugin_id])
+
+    def plugin_query(self, name, service_name=None, route_name=None, consumer_name=None):
         """
         Query Kong for a Plugin matching the given properties.
-        Raises requests.HTTPError and ValueError.
 
-        :param plugin_id: 'id' field (UUID)
-        :type plugin_id: str
-        :param name: 'name' field
+        Raises requests.HTTPError and ValueError.
+        Any given Service, Route or Consumer names are resolved to IDs
+        before filtering the Plugin list.
+
+        :param name: Plugin name (type)
         :type name: str
         :param service_name: name of the Service to resolve
         :type service_name: str
-        :param route_attrs: attributes of the Route to resolve
-        :type route_attrs: dict
+        :param route_name: name of the Route to resolve
+        :type route_name: str
         :param consumer_name: name of the Consumer to resolve
         :type consumer_name: str
-        :return: dictionary with 'total' and 'data' keys
-        :rtype: dict
+        :return: list of Plugins
+        :rtype: list
         """
+        if name is None:
+            raise ValueError("'name' is required")
 
-        if plugin_id is name is None:
-            raise ValueError("Need at least one of 'plugin_id' or 'name'")
+        service_id = None
+        route_id = None
+        consumer_id = None
 
-        params = {
-            'name': name
-        }
-
-        if plugin_id is not None:
-            # Validate the given UUID, can raise ValueError
-            # Querying for an invalid UUID will return a 400
-            uuid.UUID(plugin_id)
-            params['id'] = plugin_id
-
-        # Resolve service_name to a Service ID
-        if service_name is not None:
+        # Resolve service_name to a Service ID.
+        if service_name:
             s = self.service_get(service_name)
-
             if s is None:
                 raise ValueError(
-                    "Service '{}' not found. Has it been created?".format(service_name))
+                    "Service '{}' not found.".format(service_name))
 
             service_id = s.get('id')
-
             uuid.UUID(service_id)
 
-            params['service_id'] = service_id
-
-        # Resolve route attribute dictionary to a Route ID
-        if route_attrs is not None:
-            r = self.route_query(**route_attrs)
-
+        # Resolve route_name to a Route ID.
+        if route_name:
+            r = self.route_get(route_name)
             if r is None:
-                raise ValueError(
-                    'Route with requested attributes not found. Has it been created?')
+                raise ValueError("Route '{}' not found.".format(route_name))
 
             route_id = r.get('id')
-
             uuid.UUID(route_id)
 
-            params['route_id'] = route_id
-
-        # Resolve consumer_name to a Consumer ID
-        # consumer_name can be False, in which case the result is filtered to
-        # only contain plugins without `consumer_id`s.
+        # Resolve consumer_name to a Consumer ID.
         if consumer_name:
             c = self.consumer_get(consumer_name)
-
             if c is None:
                 raise ValueError(
-                    'Consumer {} not found. Has it been created?'.format(consumer_name))
+                    "Consumer '{}' not found.".format(consumer_name))
 
             consumer_id = c.get('id')
-
             uuid.UUID(consumer_id)
 
-            params['consumer_id'] = consumer_id
+        plugins = self.plugin_list()
 
-        # Can raise requests.HTTPError
-        p = self._get('plugins').get('data', None)
+        out = []
 
-        # Remove plugin entries that have a `consumer_id` set
-        if not consumer_name:
-            p = [x for x in p if not x.get('consumer_id')]
+        for p in plugins:
 
-        # Remove plugin entries that have a `service_id` set
-        if not service_name:
-            p = [x for x in p if not x.get('service_id')]
+            p_consumer = p.get('consumer')
+            p_service = p.get('service')
+            p_route = p.get('route')
 
-        # Remove plugin entries that have a `route_id` set
-        if not route_attrs:
-            p = [x for x in p if not x.get('route_id')]
+            if p.get('name') != name:
+                continue
 
-        return p
+            # Require the Plugin's consumer to be set if consumer is provided.
+            if (p_consumer is None) != (consumer_id is None):
+                continue
+            # Require the Plugin's consumer ID to match if given.
+            if p_consumer and p_consumer.get('id') != consumer_id:
+                continue
+
+            # Require the Plugin's service to be set if service is provided.
+            if (p_service is None) != (service_id is None):
+                continue
+            # Require the Plugin's service ID to match if given.
+            if p_service and p_service.get('id') != service_id:
+                continue
+
+            # Require the Plugin's route to be set if route is provided.
+            if (p_route is None) != (route_id is None):
+                continue
+            # Require the Plugin's route ID to match if given.
+            if p_route and p_route.get('id') != route_id:
+                continue
+
+            out.append(p)
+
+        return out
 
     def plugin_apply(self, name, config=None, service_name=None,
-                     route_attrs=None, consumer_name=False):
+                     route_name=None, consumer_name=False):
         """
-        Idempotently apply the Plugin configuration on the server.
-        See Kong API documentation for more info on the arguments of this method.
-
-        We want to manage one resource at a time only. If consumer_name is not given
-        to this function, we want to eliminate entries from the plugin query that have
-        `consumer_id` set. This behaviour is triggered by setting `consumer_name=False`
-        to plugin_query().
+        Apply the given Plugin configuration.
 
         :param name: name of the Plugin to configure
         :type name: str
         :param config: configuration parameters for the Plugin
         :type config: dict
-        :param service_name: name of the Service to configure the Plugin on
+        :param service_name: name of the Service to configure the Plugin for
         :type service_name: str
-        :param route_attrs: attributes of the Route to configure the Plugin on
-        :type route_attrs: dict
+        :param route_name: name of the Route to configure the Plugin for
+        :type route_name: str
         :param consumer_name: name of the Consumer to configure the plugin for
         :type consumer_name: str
-        :return: whether the Plugin resource was touched or not
-        :rtype: bool
+        :return: the resulting Plugin object
+        :rtype: dict
         """
-
-        uri = ['plugins']
+        if config and not isinstance(config, dict):
+            raise ValueError("'config' parameter is not a dict")
 
         data = {
             'name': name,
+            'config': config,
         }
 
         if service_name:
-            uri = ['services', service_name, 'plugins']
-
-            # Check if Service exists
-            if self.service_get(service_name) is None:
+            s = self.service_get(service_name)
+            if s is None:
                 raise ValueError(
-                    "Service '{}' not found. Has it been created?".format(service_name))
+                    "Service '{}' not found.".format(service_name))
 
-        if route_attrs:
-            r = self.route_query(**route_attrs)
+            data['service'] = s
 
+        if route_name:
+            r = self.route_get(route_name)
             if r is None:
-                raise ValueError(
-                    'Route with requested attributes not found. Has it been created?')
+                raise ValueError("Route '{}' not found.".format(route_name))
 
-            route_id = r.get('id')
-
-            uuid.UUID(route_id)
-
-            uri = ['routes', route_id, 'plugins']
+            data['route'] = r
 
         if consumer_name:
             c = self.consumer_get(consumer_name)
-
             if c is None:
                 raise ValueError(
-                    "Consumer '{}' not found. Has it been created?".format(consumer_name))
+                    "Consumer '{}' not found.".format(consumer_name))
 
-            consumer_id = c.get('id')
+            data['consumer'] = c
 
-            uuid.UUID(consumer_id)
-
-            data['consumer_id'] = consumer_id
-
-        if config is not None:
-            if not isinstance(config, dict):
-                raise ValueError("'config' parameter is not a dict")
-
-            # Merge config entries into payload
-            data.update(self._prepare_config(config))
-
-        # Query the plugin with the given criteria
+        # Query the plugin with the given attributes.
         p = self.plugin_query(name=name, service_name=service_name,
-                              route_attrs=route_attrs, consumer_name=consumer_name)
+                              route_name=route_name, consumer_name=consumer_name)
+
         if len(p) > 1:
-            raise ValueError("Found multiple Plugin records for name: '{}', service: '{}', route: '{}', consumer: '{}'".
-                             format(name, service_name, route_attrs, consumer_name))
+            raise ValueError("Multiple Plugin records for name: '{}', service: '{}', route: '{}', consumer: '{}'".
+                             format(name, service_name, route_name, consumer_name))
 
-        # Set the Plugin ID in the PUT request if the Plugin entry already exists
         if p:
-            data['id'] = p[0].get('id')
-            data['created_at'] = p[0].get('created_at')
+            # Update existing Plugin.
+            return self._patch(['plugins', p[0].get('id')], data=data)
 
-        r = self._put(uri, data=data)
+        # Insert new Plugin.
+        return self._post(['plugins'], data=data)
 
-        return r
-
-    def plugin_delete(self, name, service_name=None, route_attrs=None, consumer_name=False):
+    def plugin_delete(self, name, service_name=None, route_name=None, consumer_name=False):
         """
-        Delete the API if it exists.
+        Delete the Plugin if it exists.
 
-        :param name: name of the API to remove the Plugin configuration from
+        :param name: name (type) of the Plugin
         :type name: str
         :param consumer_name: name of the Consumer to delete the Plugin from
         :type consumer_name: str
-        :param route_attrs: attributes of the Route to delete the Plugin from
-        :type route_attrs: dict
+        :param route_name: name of the Route to delete the Plugin from
+        :type route_name: str
         :param service_name: name of the Service to delete the Plugin from
         :type service_name: str
         :return: True on a successful delete, False if no action taken
         :rtype: bool
         """
-
         p = self.plugin_query(name=name, service_name=service_name,
-                              route_attrs=route_attrs, consumer_name=consumer_name)
+                              route_name=route_name, consumer_name=consumer_name)
+
         if len(p) > 1:
             raise ValueError("Found multiple Plugin records for name: '{}', service: '{}', route: '{}', consumer: '{}'".
-                             format(name, service_name, route_attrs, consumer_name))
+                             format(name, service_name, route_name, consumer_name))
 
-        # Delete the Plugin configuration if it exists
         if p:
             return self._delete(['plugins', p[0].get('id')])
 
